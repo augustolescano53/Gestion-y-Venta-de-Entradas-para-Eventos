d40dc4c830481943949559b1e7081ca1b260fe43
jmenals <jimerodriguez2005@hotmail.com.ar>
2026-08-09 16:52:07 -0300

    Arreglo de formadepago.routes

--- backend/src/formadepago/formadepago.routes.ts
+++ backend/src/formadepago/formadepago.routes.ts
@@ -1,66 +1,11 @@
-import { Request, Response, NextFunction } from "express"
-import { FormaDePagoRepository } from "./formadepago.repository.js"
-import { FormaDePago } from "./formadepago.entity.js"
+import { Router } from "express";
+import { sanitizeFormaDePagoInput, findAll, findOne, add, update, remove } from "./formadepago.controller.js";
 
-const repository = new FormaDePagoRepository()
+export const formadepagoRouter = Router()
 
-function sanitizeFormaDePagoInput(req: Request, res: Response, next: NextFunction){
-  req.body.sanitizedInput = {
-    FormaDePagoClass: req.body.FormaDePagoClass,
-    id: req.body.id,
-    tipo: req.body.tipo,
-  }
-
-  Object.keys(req.body.sanitizedInput).forEach((key) =>{
-    if(req.body.sanitizedInput[key]===undefined){
-      delete req.body.sanitizedInput[key]}
-  })
-
-  next()
-}
-
-function findAll(req: Request,res: Response) {
-  res.json({ data: repository.findAll() })
-}
-
-function findOne(req: Request<{id: string}>, res: Response) {
-  const formadepago = repository.findOne({ id : req.params.id })
-  if(!formadepago){
-    return res.status(404).send({message: 'Forma de Pago not found'})
-  }
-  res.json({data: formadepago})
-}
-
-function add(req: Request, res: Response) {
-   const input = req.body.sanitizedInput
-
-   const formadepagoInput = new FormaDePago(
-    input.id,
-    input.tipo
-   )
-   
-   const formadepago = repository.add(formadepagoInput)
-   res.status(201).send({message: 'Forma de Pago created', data: formadepago})
-}
-
-function update(req: Request,res: Response){
-  req.body.sanitizedInput.id = req.params.id
-  const formadepago = repository.update(req.body.sanitizedInput)
-  if(!formadepago){
-    return res.status(404).send({message: 'Forma de Pago not found'})
-  }
-  return res.status(200).send({message: 'Forma de Pago updated successfully', data: formadepago})
-}
-
-function remove(req: Request<{id: string}>,res: Response){
-  const id = req.params.id
-  const formadepago = repository.delete({ id })
-
-  if(!formadepago){
-    res.status(404).send({message: 'Forma de Pago not found'})
-  } else {
-    res.status(200).send({message:'Forma de Pago deleted successfully'})
-  }
-}
-
-export {sanitizeFormaDePagoInput, findAll, findOne, add, update, remove}
+formadepagoRouter.get('/', findAll)
+formadepagoRouter.get('/:id', findOne)
+formadepagoRouter.post('/', sanitizeFormaDePagoInput, add)
+formadepagoRouter.put('/:id', sanitizeFormaDePagoInput, update)
+formadepagoRouter.patch('/:id', sanitizeFormaDePagoInput, update)
+formadepagoRouter.delete('/:id', remove)
